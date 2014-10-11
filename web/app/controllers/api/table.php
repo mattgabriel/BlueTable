4691cfcb66a7863cb3d02be4a00b42924e05651d
Brian Thai <brian@excelwithbusiness.com>
2014-10-11 22:20:48 +0100

    Updated.

--- web/app/controllers/api/table.php
+++ web/app/controllers/api/table.php
@@ -16,6 +16,7 @@ class table extends restService {
             if(!empty($data))
             {
                 $row = $data[0];
+                echo $row->TableStatus;
                 if($row->TableStatus == TableModel::TABLE_STATUS_PAID)
                     $model->update(array('AutoId'=>$row->AutoId, 'TableStatus'=>TableModel::TABLE_STATUS_AWAITING_CLEANING));
             }
