97ffb6d69648fdcc5119b259c9dda7116f180d94
mattgabriel <matt_martire@yahoo.co.uk>
2014-10-12 06:43:55 +0100

    app updates

---
+++ app/BlueTable/BlueTable/SuccessViewController.h
@@ -0,0 +1,18 @@
+//
+//  SuccessViewController.h
+//  BlueTable
+//
+//  Created by Matt Gabriel on 12/10/2014.
+//  Copyright (c) 2014 Matt Gabriel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuccessViewController : UIViewController
+
+
+@property (strong, nonatomic) NSString *UserId;
+@property (strong, nonatomic) NSString *tableId;
+@property (strong, nonatomic) NSString *orderId;
+
+@end
