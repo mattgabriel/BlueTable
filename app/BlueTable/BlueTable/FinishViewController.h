00ca50ac11138358d92302f9feca445810a6bc59
mattgabriel <matt_martire@yahoo.co.uk>
2014-10-12 08:08:36 +0100

    app updates

---
+++ app/BlueTable/BlueTable/FinishViewController.h
@@ -0,0 +1,18 @@
+//
+//  FinishViewController.h
+//  BlueTable
+//
+//  Created by Matt Gabriel on 12/10/2014.
+//  Copyright (c) 2014 Matt Gabriel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FinishViewController : UIViewController <NSURLConnectionDelegate> {
+    NSMutableData *responseData;
+}
+
+@property (strong, nonatomic) NSString *orderId;
+
+
+@end
