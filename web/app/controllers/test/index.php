40418b48e25415810880b558f6c5bbc331fe57d5
Brian Thai <brian@excelwithbusiness.com>
2014-10-12 06:40:09 +0100

    Added send grid and fixed api.

--- web/app/controllers/test/index.php
+++ web/app/controllers/test/index.php
@@ -4,13 +4,15 @@ require_once APP_PATH.'controllers/controllerObject.php';
 require_once APP_PATH.'library/paypal.php';
 require_once APP_PATH.'library/justgiving.php';
 require_once APP_PATH.'library/paypal.php';
+require_once APP_PATH.'library/sendgrid.php';
 
 class index extends controllerObject{
     protected function _setComponentName() {
         
     }
     function __construct() {
-        $jg = new paypal();
+        $sg = new sendgrid();
+        $sg->send('test');
     }
 
 }
